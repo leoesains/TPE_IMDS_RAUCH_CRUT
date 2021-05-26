d7bc1019b9d75992748e21303011865e8e4f1955
Joaquin Loiza <loiza.joaquin@gmail.com>
2021-05-25 23:14:41 -0300

    se creo la funcion que setea la ruta junto con un nombre unico y la extension para almacenar el archivo en esa esa ruta.

--- models/aviso.model.php
+++ models/aviso.model.php
@@ -5,11 +5,21 @@ require_once 'models/db.conection.model.php';
 class AvisoModel extends dbConectionModel {
 
     //ingresa un nuevo aviso a la BBDD
-    public function insert($nombre, $apellido, $direccion, $telefono, $email, $franja_horaria, $volumen, $foto) {
+    public function insert($nombre, $apellido, $direccion, $telefono, $email, $franja_horaria, $volumen, $imagen,$nombre_imagen) {
+        $path_img = null;
+        if($imagen){
+            $path_img = $this->uploadImage($imagen, $nombre_imagen);
+        }
         //enviamos la consulta
         $sql = "INSERT INTO aviso_de_retiro(nombre, apellido, direccion, telefono, email, franja_horaria, volumen, fotografia) 
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?) ";
         $query = $this->getConnection()->prepare($sql);  
-        $query->execute([$nombre, $apellido, $direccion, $telefono, $email, $franja_horaria, $volumen, $foto]);        
+        $query->execute([$nombre, $apellido, $direccion, $telefono, $email, $franja_horaria, $volumen, $path_img]);        
+    }
+
+    private function uploadImage($imagen, $nombre_imagen){
+        $target = 'uploads/images/' . uniqid("", true) . "." . strtolower(pathinfo($nameImg, PATHINFO_EXTENSION));
+        move_uploaded_file($imagen, $target);
+        return $target;
     }
 }

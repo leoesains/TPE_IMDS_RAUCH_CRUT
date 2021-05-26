1984c276afd39cd17e1000e32266bcc423dc784d
Joaquin Loiza <loiza.joaquin@gmail.com>
2021-05-25 23:13:21 -0300

    CRUT-85 desarrollo para procesar los datos de retiro, verificando que el pedido es válido en relación a la distancia domicilio_ciudadano/domicilio_cooperativa

--- controllers/PublicController.php
+++ controllers/PublicController.php
@@ -7,6 +7,9 @@ class PublicController{
 
     //private $materialesModel; 
     private $view;
+    private const $minRand = 1;
+    private const $maxRand = 12;
+    private const $maxKmPermitios = 6;
 
     public function __construct() {
        // $this->materialesModel = new MaterialesMode();
@@ -46,5 +49,39 @@ class PublicController{
         echo($error);
     }
 
+    public function addAviso(){
+        $nombre = $_POST['nombre'];
+        $apellido=$_POST['apellido'];
+        $direccion=$_POST['direccion'];
+        $telefono=$_POST['telefono'];
+        $email=$_POST['email'];
+        $franja_horaria=$_POST['franja_horaria'];
+        $volumen=$_POST['volumen'];
+        $imagen=$_FILES['input_name']['tmp_name'];
+        $nombre_imagen=$_FILES['input_name']['name'];
+
+        if(!empty($nombre) && !empty($apellido) && !empty($direccion) && !empty($telefono)&& !empty($email) && !empty($franja_horaria) && !empty($volumen)
+        && verificarDistancia() == true){
+            $this->model->insert($nombre,$apellido,$direccion,$telefono,$email,$franja_horaria,$volumen,$imagen,$nombre_imagen);
+            header('location:'.BASE_URL.'avisos');
+        }
+        else if(verificarDistancia() == false) {
+            $this->errorview->showError('La distancia de su domicilio a la planta supera los '+$maxKmPermitios+'Km');
+        }
+        else{
+            $this->errorview->showError('Existen uno o mas campos obligatorios vacios');
+        }
+    }
+
+    public function verificarDistancia(){
+        
+        $num = rand($minRand ,$maxRand);
+
+        if($num > $maxKmPermitios){
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
 

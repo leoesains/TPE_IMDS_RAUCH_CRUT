1236e12016963ebe05778546f353df65bc917fb4
Marcela Aguirre <aguirremarcela@hotmail.com.ar>
2021-06-18 12:16:42 -0300

    crut-106 -parámetros en la fórmula del modelo

--- models/aviso.model.php
+++ models/aviso.model.php
@@ -5,16 +5,16 @@ require_once 'models/db.conection.model.php';
 class AvisoModel extends dbConectionModel {
 
     //ingresa un nuevo aviso a la BBDD
-    public function insert($nombre, $apellido, $direccion, $telefono, $email, $franja_horaria, $volumen, $imagen,$nombre_imagen) {
+    public function insert($nombre, $apellido, $direccion, $telefono, $email, $franja_horaria, $volumen, $imagen,$nombre_imagen,$estado,$fecha_entrega) {
         $path_img = null;
         if($imagen){
             $path_img = $this->uploadImage($imagen, $nombre_imagen);
         }
         //enviamos la consulta
-        $sql = "INSERT INTO aviso_de_retiro(nombre, apellido, direccion, telefono, email, franja_horaria, volumen, fotografia) 
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?) ";
+        $sql = "INSERT INTO aviso_de_retiro(nombre, apellido, direccion, telefono, email, franja_horaria, volumen, fotografia,estado,fecha_entrega) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?,?,?) ";
         $query = $this->getConnection()->prepare($sql);  
-        $query->execute([$nombre, $apellido, $direccion, $telefono, $email, $franja_horaria, $volumen, $path_img]);        
+        $query->execute([$nombre, $apellido, $direccion, $telefono, $email, $franja_horaria, $volumen, $path_img,$estado,$fecha_entrega]);        
     }
 
     private function uploadImage($imagen, $nombre_imagen){
